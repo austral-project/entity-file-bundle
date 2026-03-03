0cb77e54ee01bb0ef2cf0d14e7356839b7e6a320
Matthieu Beurel <matthieu@mbeurel.com>
2026-03-03 09:15:03 +0100

    Update Compression.php to add API-based compression support
    - **File Modified:**
      `Compression.php`
    
    - Added `executeApi` method to handle API-based image compression.
    - Refactored `executeCommand` method to separate command and API logic.
    - Integrated HTTP client for API calls and exception handling for errors.
    - Enhanced compatibility for multiple formats during compression.

--- File/Compression/Compression.php
+++ File/Compression/Compression.php
@@ -11,7 +11,12 @@
 namespace Austral\EntityFileBundle\File\Compression;
 
 use Austral\EntityFileBundle\Configuration\CompressionConfiguration;
+use Austral\ToolsBundle\AustralTools;
+use Symfony\Component\HttpClient\NativeHttpClient;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Process\Process;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 
 /**
  * Austral Compression.
@@ -52,46 +57,113 @@ class Compression
   {
     if($command = $this->compressionConfiguration->get('command', null))
     {
-      $imageThumbnailPathDir = pathinfo($imageThumbnailPath, PATHINFO_DIRNAME);
-      $extension = pathinfo($imageThumbnailPath, PATHINFO_EXTENSION);
-
-      if($squooshFormat = $this->getSquooshOptionByFormat($extension))
+      if(str_contains($command, "api://") === true)
       {
-        $commandParameters = array(
-          $command,
-          "--{$squooshFormat}"
-        );
-        $optionsBySquooshFormat = $this->compressionConfiguration->get("options.{$squooshFormat}", null);
-        $optionsBySquooshFormatJson = $optionsBySquooshFormat ? json_encode($optionsBySquooshFormat) : "{}";
-        $commandParameters[] = $optionsBySquooshFormatJson;
-        if($generateOtherFormat)
-        {
-          foreach($generateOtherFormat as $format)
-          {
-            if($otherSquooshFormat = $this->getSquooshOptionByFormat($format))
-            {
-              $otherOptionsBySquooshFormat = $this->compressionConfiguration->get("options.{$otherSquooshFormat}", null);
-              $otherOptionsBySquooshFormatJson = $otherOptionsBySquooshFormat ? json_encode($otherOptionsBySquooshFormat) : "{}";
-              $commandParameters[] = "--{$otherSquooshFormat}";
-              $commandParameters[] = $otherOptionsBySquooshFormatJson;
-            }
-          }
-        }
-        $commandParameters[] = "-d";
-        $commandParameters[] = $imageThumbnailPathDir;
-        $commandParameters[] = $imageThumbnailPath;
-
-        $commandParameters[]  = ">";
-        $commandParameters[]  = "/dev/null";
-        $commandParameters[]  = "2>&1";
-        $commandParameters[]  = "&";
-
-        $process = Process::fromShellCommandline(implode(" ", $commandParameters));
-        $process->setTimeout(120);
-        $process->run();
+        $this->executeApi($command, $imageThumbnailPath, $generateOtherFormat);
+      }
+      else
+      {
+        $this->executeCommand($command, $imageThumbnailPath, $generateOtherFormat);
       }
     }
   }
+
+  protected function executeCommand(string $command, string $imageThumbnailPath, array $generateOtherFormat = array())
+  {
+    $imageThumbnailPathDir = pathinfo($imageThumbnailPath, PATHINFO_DIRNAME);
+    $extension = pathinfo($imageThumbnailPath, PATHINFO_EXTENSION);
+
+    if($squooshFormat = $this->getSquooshOptionByFormat($extension))
+    {
+      $commandParameters = array(
+        $command,
+        "--{$squooshFormat}"
+      );
+      $optionsBySquooshFormat = $this->compressionConfiguration->get("options.{$squooshFormat}", null);
+      $optionsBySquooshFormatJson = $optionsBySquooshFormat ? json_encode($optionsBySquooshFormat) : "{}";
+      $commandParameters[] = $optionsBySquooshFormatJson;
+      if($generateOtherFormat)
+      {
+        foreach($generateOtherFormat as $format)
+        {
+          if($otherSquooshFormat = $this->getSquooshOptionByFormat($format))
+          {
+            $otherOptionsBySquooshFormat = $this->compressionConfiguration->get("options.{$otherSquooshFormat}", null);
+            $otherOptionsBySquooshFormatJson = $otherOptionsBySquooshFormat ? json_encode($otherOptionsBySquooshFormat) : "{}";
+            $commandParameters[] = "--{$otherSquooshFormat}";
+            $commandParameters[] = $otherOptionsBySquooshFormatJson;
+          }
+        }
+      }
+      $commandParameters[] = "-d";
+      $commandParameters[] = $imageThumbnailPathDir;
+      $commandParameters[] = $imageThumbnailPath;
+
+      $commandParameters[]  = ">";
+      $commandParameters[]  = "/dev/null";
+      $commandParameters[]  = "2>&1";
+      $commandParameters[]  = "&";
+
+      $process = Process::fromShellCommandline(implode(" ", $commandParameters));
+      $process->setTimeout(120);
+      $process->run();
+    }
+  }
+
+  protected function executeApi(string $command, string $imageThumbnailPath, array $generateOtherFormat = array())
+  {
+    $url = str_replace("api://", "http://", $command)."/optimize";
+    $imageThumbnailPathDir = pathinfo($imageThumbnailPath, PATHINFO_DIRNAME);
+    $extension = pathinfo($imageThumbnailPath, PATHINFO_EXTENSION);
+    $formats = array();
+    $options = array();
+    if($squooshFormat = $this->getSquooshOptionByFormat($extension))
+    {
+      $formats[] = $squooshFormat;
+      $options[$squooshFormat] = $this->compressionConfiguration->get("options.{$squooshFormat}", array());
+    }
+    if($generateOtherFormat)
+    {
+      foreach ($generateOtherFormat as $format)
+      {
+        if ($otherSquooshFormat = $this->getSquooshOptionByFormat($format))
+        {
+          $formats[] = $otherSquooshFormat;
+          $options[$squooshFormat] = $this->compressionConfiguration->get("options.{$otherSquooshFormat}", array());
+        }
+      }
+    }
+    if(count($formats) > 0) {
+      $httpClient = new NativeHttpClient();
+      $payload = [
+        'filename' => $imageThumbnailPath,
+        'outputDir' => $imageThumbnailPathDir,
+        'formats' => $formats,
+        'options' => $options,
+      ];
+
+      try {
+        $response = $httpClient->request('POST', $url, [
+          'headers' => [
+            'Content-Type' => 'application/json',
+          ],
+          'body' => json_encode($payload),
+          'timeout' => 30,
+        ]);
+
+        $statusCode = $response->getStatusCode();
+        $content = $response->getContent();
+
+      } catch (TransportExceptionInterface|ClientExceptionInterface|ServerExceptionInterface $e) {
+        throw new \RuntimeException("Erreur HTTP: " . $e->getMessage());
+      }
+      $data = json_decode($content, true);
+      if ($statusCode !== 200 || !$data || !isset($data['accepted'])) {
+        throw new \RuntimeException("Réponse invalide du service Squoosh : $content");
+      }
+    }
+  }
+
 
   /**
    * @param string $extension
